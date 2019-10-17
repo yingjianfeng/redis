913d3af220a1725ad1a0830440b58a097a5e6065
yjf <1374993958@qq.com>
2019-10-17 14:13:27 +0800

    add1

--- sourcetree_test/src/com/Test.java
+++ sourcetree_test/src/com/Test.java
@@ -3,8 +3,7 @@ package com;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("add");//03
+		System.out.println("add1");//
 	}
 
 }
